f80ce72ef9bfded2c7d722cbf22bcea995d8f2ad
Coty Embry <Coty Embry>
2017-04-10 18:47:56 -0500

    added js files

--- simpleWebsite/App1/views.py
+++ simpleWebsite/App1/views.py
@@ -6,17 +6,40 @@ import datetime
 # Create your views here.
 
 
+# this works
+#
 def index(request):
-    return render_to_response('D:\\Developer\\DjangoWebserver\\simpleWebsite\\index.html')
-    # return render_to_response('simpleWebsite/index.html')
+	# content = '<html>test123</html>'
+
+	# response = HttpResponse(content, content_type='application/liquid')
+	# response['Content-Length'] = len(content)
+
+	# return response
+
+
+					# <script src=\"https://raw.githubusercontent.com/cotyembry/DjangoWebserver/master/simpleWebsite/App1/templates/index.html\"></script>\
+
+	html = "<html>\
+				<body>\
+					<script src=\"http://rawgit.com/cotyembry/DjangoWebserver/master/simpleWebsite/App1/templates/index.html\"></script>\
+				</body>\
+			</html>"
+
+	response = HttpResponse(html, content_type='text/html')
+	response['Content-Length'] = len(html)
+
+	return response
+
 
 # def index(request):
 #     return HttpResponse("Boom, in App1 app's index view")
 
 
-# this works
+# I could not figure out how to get this to work - this requires more configuration
 #
 # def index(request):
-#     now = datetime.datetime.now()
-#     html = "<html><body><b>It is now %s.</b></body></html>" % now
-#     return HttpResponse(html)
+#     return render_to_response('D:\\Developer\\DjangoWebserver\\simpleWebsite\\index.html')
+#     # return render_to_response('simpleWebsite/index.html')
+
+
+
